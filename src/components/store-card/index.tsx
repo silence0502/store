21d7893b031f4947056f8e596e70e84a24a756d5
小志 <379658593@qq.com>
2018-08-09 18:42:27 +0800

    添加图片

--- src/components/store-card/index.tsx
+++ src/components/store-card/index.tsx
@@ -4,16 +4,22 @@ import { Card, Icon } from 'antd';
 import './style/index';
 
 export interface StoreCardProps {
-    data?: any;
+    data?: any
+    renderModal?: any
 }
 class StoreCard extends React.Component<StoreCardProps, any> {
+    renderModal() {
+        let id = this.props.data.id
+        if (this.props.renderModal) {
+            this.props.renderModal(id)
+        }
+    }
     render() {
         let { data } = this.props;
         return (
-            <div className="store-card">
+            <div className="store-card" >
                 <Card
-
-                    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                    cover={<img alt="example" src={data.images} onClick={this.renderModal.bind(this)} />}
                     actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
                 >
                 </Card>
--- src/modules/store/views/store.tsx
+++ src/modules/store/views/store.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Pagination, Input, Row, Col } from 'antd';
+import { Pagination, Input, Row, Col, Modal } from 'antd';
 const Search = Input.Search;
 
 import SectionHeader from '../../../components/section-header'
@@ -12,12 +12,66 @@ export interface StoreProps {
     match?
     location?
 }
+let data = [
+    {
+        id: 0,
+        images: require('../../../img/1-1.JPG')
+    },
+    {
+        id: 1,
+        images: require('../../../img/1-2.JPG')
+    },
+    {
+        id: 2,
+        images: require('../../../img/1-3.JPG')
+    },
+    {
+        id: 3,
+        images: require('../../../img/1-4.JPG')
+    },
+    {
+        id: 4,
+        images: require('../../../img/1-5.JPG')
+    },
+    {
+        id: 5,
+        images: require('../../../img/1-6.JPG')
+    },
+    {
+        id: 6,
+        images: require('../../../img/1-6的侧.JPG')
+    },
+    {
+        id: 7,
+        images: require('../../../img/1-7.JPG')
+    },
+]
 class Store extends React.PureComponent<StoreProps, any> {
     constructor(props: any) {
         super(props);
         this.state = {
-
+            visible: false,
+            _data: {}
         };
+    }
+
+    handleOk = (e) => {
+        this.setState({
+            visible: false,
+        });
+    }
+
+    handleCancel = (e) => {
+        this.setState({
+            visible: false,
+        });
+    }
+
+    renderModal(id) {
+        this.setState({
+            visible: true,
+            _data: data[id]
+        })
     }
 
     componentDidMount() {
@@ -30,51 +84,53 @@ class Store extends React.PureComponent<StoreProps, any> {
     componentWillUnmount() {
 
     }
+    renderCard() {
+        return (
+            data.map((item, index) => {
+                return <Col className="gutter-row" span={6}>
+                    <div className="gutter-box" style={{ marginBottom: '10px' }}><StoreCard data={item} renderModal={this.renderModal.bind(this)} /></div>
+                </Col>
+            })
+        )
+    }
     renderList() {
         return (
             <div>
-                <Row gutter={16}>
-                    <Col className="gutter-row" span={6}>
-                        <div className="gutter-box"><StoreCard /></div>
-                    </Col>
-                    <Col className="gutter-row" span={6}>
-                        <div className="gutter-box"><StoreCard /></div>
-                    </Col>
-                    <Col className="gutter-row" span={6}>
-                        <div className="gutter-box"><StoreCard /></div>
-                    </Col>
-                    <Col className="gutter-row" span={6}>
-                        <div className="gutter-box"><StoreCard /></div>
-                    </Col>
-                    <Col className="gutter-row" span={6}>
-                        <div className="gutter-box"><StoreCard /></div>
-                    </Col>
-                    <Col className="gutter-row" span={6}>
-                        <div className="gutter-box"><StoreCard /></div>
-                    </Col>
-                    <Col className="gutter-row" span={6}>
-                        <div className="gutter-box"><StoreCard /></div>
-                    </Col>
-                    <Col className="gutter-row" span={6}>
-                        <div className="gutter-box"><StoreCard /></div>
-                    </Col>
+                <Row gutter={15}>
+                    {this.renderCard()}
                 </Row>
             </div>
         )
     }
     render() {
+        let { _data } = this.state
         return (
             <div>
                 <SectionHeader title='图像列表'></SectionHeader>
-                <div style={{ width: '100%' }}>
+                <div style={{ width: '100%', overflow: 'hidden' }}>
                     <Search
                         placeholder="请输入图片名称"
-                        style={{ width: 200, float: 'right', marginRight: '15px' }}
+                        style={{ width: 200, float: 'right', margin: ' 0 0 15px 0' }}
                     />
                 </div>
                 {this.renderList()}
-                <div style={{ marginTop: '10px', textAlign: 'center' }}>
+                <div style={{ margin: '15px 0 0 0', textAlign: 'center' }}>
                     <Pagination showQuickJumper defaultCurrent={2} total={500} />
+                </div>
+                <div>
+                    <Modal
+                        title=""
+                        bodyStyle={{
+                            height: '400px'
+                        }}
+                        width={700}
+                        visible={this.state.visible}
+                        onOk={this.handleOk}
+                        onCancel={this.handleCancel}
+                    >
+                        <img alt="example" src={_data.images} style={{ width: '200px', height: '200px' }} />
+                        <p>Some contents...</p>
+                    </Modal>
                 </div>
             </div>
         )
